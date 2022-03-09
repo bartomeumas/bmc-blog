edd25573d04813c3d22d0c03dbd8d4e2c051d25c
bartomeumas <bartomeumas00@gmail.com>
2022-03-09 15:55:40 -0400

    fixed index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import { FeaturedPosts } from '../sections/index'
 import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
-import { FeaturedPosts } from '../sections/index'
 
-const Home: NextPage = ({ posts }) => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto mb-8 px-10">
-      <Head>
-        <title>BMC Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
-            <PostCard post={post.node} key={post.title} />
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
@@ -29,11 +23,9 @@ const Home: NextPage = ({ posts }) => {
   )
 }
 
-export default Home
-
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || []
-
   return {
     props: { posts },
   }
